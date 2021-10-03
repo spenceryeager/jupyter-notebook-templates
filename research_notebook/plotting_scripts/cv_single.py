1fdaa488e85e425e5a1da4b1e71927e5870d7497
Spencer Yeager <yeager.spencer@gmail.com>
2021-10-02 22:19:02 -0700

    scripts

--- research_notebook/plotting_scripts/cv_single.py
+++ research_notebook/plotting_scripts/cv_single.py
@@ -1,7 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
 workingfile = ENTER FILE
 data = pd.read_csv(workingfile, skiprows=ENTER NUMBER)
 plt.plot(data['Potential/V'], data[' Current/A'], color='red')
